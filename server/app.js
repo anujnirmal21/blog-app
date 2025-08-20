83b6613e89155a84c060b9113158b893e53b4319
Anuj Nirmal <anunirmal5648@gmail.com>
2025-08-20 13:54:13 +0530

    deploy done

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.routes.js";
 import blogRoutes from "./routes/blog.routes.js";
 
 const app = express();
+const CLIENT_URL = process.env.CLIENT_URL;
 
 //middlewares
 app.use(
@@ -19,7 +20,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://blog03.netlify.app",
+    origin: CLIENT_URL,
     credentials: true,
   })
 );
